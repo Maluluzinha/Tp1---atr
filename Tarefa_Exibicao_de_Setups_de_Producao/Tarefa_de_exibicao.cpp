9fcd92f5eec04d88428da6dec0f841deb55eb78a
Maria Luisa <malulu1604@gmail.com>
2024-12-21 00:45:26 -0300

    Versão sem comentarios

--- Tarefa_Exibicao_de_Setups_de_Producao/Tarefa_de_exibicao.cpp
+++ Tarefa_Exibicao_de_Setups_de_Producao/Tarefa_de_exibicao.cpp
@@ -1,67 +1,3 @@
-//#define WIN32_LEAN_AND_MEAN
-//#include <windows.h>
-//#include <iostream>
-//#include <string>
-//#include <mutex>
-//#include <thread>
-//#include <chrono>
-//
-//const wchar_t* pipeName = L"\\\\.\\pipe\\MeuPipe";
-//
-//void conectarPipeEExibirDados() {
-//    HANDLE hPipe = CreateFile(
-//        pipeName,               // Nome da pipe
-//        GENERIC_READ,           // Acesso de leitura
-//        0,                      // Sem compartilhamento
-//        NULL,                   // Segurança padrão
-//        OPEN_EXISTING,          // Abre a pipe existente
-//        0,                      // Atributos padrão
-//        NULL);                  // Sem modelo de atributo
-//
-//    if (hPipe == INVALID_HANDLE_VALUE) {
-//        std::cerr << "Erro ao conectar à pipe. Código: " << GetLastError() << std::endl;
-//        return;
-//    }
-//
-//    std::cout << "Conexão à pipe estabelecida." << std::endl;
-//
-//    while (true) {
-//        char buffer[512];
-//        DWORD bytesRead;
-//
-//        // Tenta ler dados da pipe
-//        if (ReadFile(hPipe, buffer, sizeof(buffer) - 1, &bytesRead, NULL)) {
-//            buffer[bytesRead] = '\0'; // Garante que o buffer seja terminado com '\0'
-//
-//            std::cout << "Mensagem recebida da pipe: " << buffer << std::endl;
-//
-//            // Formata e exibe os dados
-//            std::cout << "HH:MM:SS " << buffer << std::endl;
-//        }
-//        else {
-//            DWORD error = GetLastError();
-//            if (error == ERROR_BROKEN_PIPE) {
-//                std::cout << "Conexão com a pipe foi encerrada pelo servidor." << std::endl;
-//                break;
-//            }
-//            else {
-//                std::cerr << "Erro ao ler da pipe. Código: " << error << std::endl;
-//            }
-//        }
-//
-//        std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Aguarda antes de tentar novamente
-//    }
-//
-//    CloseHandle(hPipe);
-//}
-//
-//int main() {
-//    std::cout << "Cliente de leitura iniciado..." << std::endl;
-//
-//    conectarPipeEExibirDados();
-//
-//    return 0;
-//}
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
@@ -88,10 +24,7 @@ HANDLE escEvent;
 std::atomic<bool> continuarProcesso{ true };
 
 void criarEventos() {
-    /*setupDisplayEvent = OpenEvent(SYNCHRONIZE, FALSE, L"setupDisplayEvent");
-    escEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, L"EscEvent");*/
-    //setupDisplayEvent = CreateEvent(NULL, TRUE, FALSE, L"setupDisplayEvent");
-
+    
     setupDisplayEvent = OpenEvent(SYNCHRONIZE | EVENT_MODIFY_STATE, FALSE, L"SetupDisplayEvent");
     escEvent = OpenEvent(SYNCHRONIZE | EVENT_MODIFY_STATE, FALSE, L"EscEvent");
 
